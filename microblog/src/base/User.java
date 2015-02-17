d1a2a0c79cc6b36f33592e381da3bc6a58388d8c
csderek <derekwong1993@gmail.com>
2015-02-17 20:17:03 +0800

    more edit

--- microblog/src/base/User.java
+++ microblog/src/base/User.java
@@ -5,6 +5,7 @@ public class User {
 	private String username;
 	private String useremail;
 	
+	//constructer
 	public User(int id, String username, String useremail){
 		this.id = id;
 		this.useremail = useremail;
@@ -25,6 +26,14 @@ public class User {
 	
 	public void setUsername(String name){
 		username = name;
+	}
+	
+	public String getUseremail(){
+		return useremail;
+	}
+	
+	public void setUseremail(String email){
+		useremail = email;
 	}
 	
 	public String toString(){
