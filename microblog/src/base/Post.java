0a2423085ddcf01f5b7d2b0023a2e1ee87224aae
csderek <derekwong1993@gmail.com>
2015-02-25 12:33:27 +0800

    finished

--- microblog/src/base/Post.java
+++ microblog/src/base/Post.java
@@ -23,8 +23,32 @@ public class Post {
 		this.content = content;
 	}
 	
+	public Date getdate(){
+		return date;
+	}
+	
+	public void setDate(Date date){
+		this.date = date;
+	}
+	
+	public User getUser(){
+		return user;
+	}
+	
+	public void setUser(User user){
+		this.user = user;
+	}
+	
 	public String toString(){
 		return user.toString() + "\n" + date + "\n" + content;
+	}
+	
+	public boolean contains(String str){
+		if (this.content.indexOf(str) < 0){
+			return false;
+		}else{
+			return true;
+		}
 	}
 	public boolean equals(Object o){
 		boolean ans = true;
--- microblog/src/main/main.java
+++ microblog/src/main/main.java
@@ -2,6 +2,7 @@ package main;
 
 import java.util.Date;
 
+import base.Friendpost;
 import base.Post;
 import base.User;
 
@@ -12,5 +13,15 @@ public class main {
 		String content = "Hello!!I am Derek!!";
 		Post post = new Post(date, content, user);
 		System.out.println(post.toString());
+		
+		
+		User user01 = new User(1, "COMP3021", "COMP3021@cse.ust.hk");
+		Date d = new Date();
+		String content01 = "This is my first post";
+		Friendpost postfromfriend = new Friendpost(d, content01, user01);
+		System.out.println(postfromfriend.toString());
+		
+		System.out.println(postfromfriend.contains("first"));
+		System.out.println(postfromfriend.contains("HKUST"));
 	}
 }
