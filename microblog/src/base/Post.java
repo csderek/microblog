a31f707c7f9054412f67a011e4dab110c39f1843
csderek <derekwong1993@gmail.com>
2015-02-25 12:36:27 +0800

    finished

--- microblog/src/base/Post.java
+++ microblog/src/base/Post.java
@@ -50,6 +50,12 @@ public class Post {
 			return true;
 		}
 	}
+	public int hashcode(){
+		int hashcode =0;
+		date.hashCode();
+		content.hashCode();
+		return hashcode;
+	}
 	public boolean equals(Object o){
 		boolean ans = true;
 		if(o == null){
